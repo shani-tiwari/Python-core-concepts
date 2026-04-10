6a7a0d0ecb6c4ff26c4cdc5529780138f7be9dd5
shani-tiwari <shanitiwari2021@gmail.com>
2026-04-10 13:16:10 +0530

    closure add

---
+++ Closure.py
@@ -0,0 +1,11 @@
+
+# Closure - nested function access the outer scope of the enclosing function
+def add_ten():
+    ten = 10
+    def add(num):
+        return num + ten
+    return add
+
+closure_result = add_ten()
+print(closure_result(5))  # 15
+print(closure_result(10))  # 20
